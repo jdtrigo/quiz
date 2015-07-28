126795e1b5bf978e4c47a694cc767a66751705b3
Jesus D. Trigo <jesus.daniel.trigo@gmail.com>
2015-07-28 15:04:11 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,6 +49,12 @@ exports.answer = function (req, res) {
   res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado, errors: []});
 };
 
+// GET quizes/:id/edit
+exports.edit = function (req, res) {
+  var quiz = req.quiz; // autoload de instancia de quiz
+  res.render('quizes/edit', {quiz:quiz, errors: []});
+};
+
 
 // GET quizes/new
 exports.new = function (req, res) {
@@ -71,6 +77,27 @@ exports.create = function (req, res) {
       res.render('quizes/new', {quiz: quiz, errors: err.errors});
     } else {
       quiz
+      .save({fields: ["pregunta", "respuesta"]})
+      .then(function(){
+        res.redirect('/quizes');
+      });
+    }
+  });
+};
+
+
+// PUT quizes/update
+exports.update = function (req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(function(err) {
+    if (err) {
+      res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+    } else {
+      req.quiz
       .save({fields: ["pregunta", "respuesta"]})
       .then(function(){
         res.redirect('/quizes');
